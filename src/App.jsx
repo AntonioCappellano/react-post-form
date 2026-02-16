241f6d900cef46eed5fc3695c4999327cc3ebc20
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-16 15:05:20 +0100

    Initialize form state and handle form data updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
+const inizialData = {
+  author: "",
+  title: "",
+  body: "",
+  public: false,
+};
+
 export default function App() {
+  const [formData, setFormData] = useState(inizialData);
+  function handleFormData(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
   return <></>;
 }
