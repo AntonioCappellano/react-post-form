2c5c3a0b357df3cdaa1d74fd968f390ff5bdacad
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-16 15:34:46 +0100

    Refactor form data handling and add textarea for post content

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,10 @@ const inizialData = {
 export default function App() {
   const [formData, setFormData] = useState(inizialData);
   function handleFormData(e) {
+    const { name, value, type, checked } = e.target;
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value,
+      [name]: type === "checkbox" ? checked : value,
     });
   }
   return (
@@ -39,7 +40,29 @@ export default function App() {
               onChange={handleFormData}
             ></input>
           </div>
-          
+          <div>
+            <label className="form-label">Post</label>
+            <textarea
+              type="text"
+              className="form-control"
+              name="body"
+              value={formData.body}
+              onChange={handleFormData}
+            ></textarea>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              name="public"
+              checked={formData.public}
+              onChange={handleFormData}
+              id="checkDefault"
+            />
+            <label class="form-check-label" htmlFor="checkDefault">
+              Pubblica
+            </label>
+          </div>
         </form>
       </div>
     </>
